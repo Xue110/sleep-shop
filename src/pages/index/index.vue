2cac708c8610ec07edd9883e4e1c8ae279cf3cf5
Xue110 <148840128+Xue110@users.noreply.github.com>
2024-07-20 21:08:17 +0800

    获取前台分类

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,15 +1,43 @@
 <script setup lang="ts">
-import CustomNavbar from './components/CustomNavbar'
+import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home';
+import CustomNavbar from './components/CustomNavbar';
+import CategoryPanel from './components/CategoryPanel';
+import { onLoad } from '@dcloudio/uni-app';
+import { ref } from 'vue';
+import type { BannerItem } from '@/types/home';
+
+//获取轮播图数据
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async() =>{
+ const res = await getHomeBannerAPI()
+ console.log(res)
+ bannerList.value = res.result
+}
+
+// 获取前台分类数据
+const getHomeCategoryData = async() =>{
+  const res = await getHomeCategoryAPI()
+  console.log(res)
+}
+
+// 页面加载
+onLoad(() => {
+  getHomeBannerData()
+  getHomeCategoryData()
+})
 </script>
 
 <template>
-<!-- 自定义导航栏 -->
+  <!-- 自定义导航栏 -->
   <CustomNavbar />
-  <XtxSwiper />
-  <view class="content">
-    <view class="title">Hello, world!</view>
-  </view>
+  <!-- 自定义轮播图-->
+  <XtxSwiper :list="bannerList" />
+  <!-- 分类面板 -->
+  <CategoryPanel />
 </template>
 
 <style lang="scss">
+page{
+  background-color: #f7f7f7;
+}
 </style>
