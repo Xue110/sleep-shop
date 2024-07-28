0d1e64a4f70a9afe18a2b38473df41a8dc356f17
Xue110 <148840128+Xue110@users.noreply.github.com>
2024-07-28 18:53:53 +0800

    购物车功能完善

--- src/services/cart.ts
+++ src/services/cart.ts
@@ -23,3 +23,40 @@ export const getCartList = () => {
     method: "GET"
   })
 }
+
+/**
+ * 删除/清空 购物车单品
+ * @params {ids: string[]} 商品skuId数组
+ */
+export const deleteMemberCartAPI = (data: { ids: string[] }) => {
+  return http({
+    method: 'DELETE',
+    url: '/member/cart',
+    data,
+  })
+}
+
+/**
+ * 修改购物车商品
+ * @params {skuId: string} 商品skuId
+ */
+export const putMemberCartBySkuIdAPI = (skuId: string, data: { selected?: boolean, count?: number }) => {
+  return http({
+    method: 'PUT',
+    url: `/member/cart/${skuId}`,
+    data,
+  })
+}
+
+/**
+ * 购物车全选
+ * @params {selected: boolean} 是否全选
+ * @returns {Promise} Promise
+ */
+export const checkAllCartAPI = (data: { selected: boolean }) => {
+  return http({
+    method: 'PUT',
+    url: '/member/cart/selected',
+    data
+  })
+}
